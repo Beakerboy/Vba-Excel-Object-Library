74fc48ab914eb2cd79928f688fb8ca2b50641269
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-11 15:12:56 -0400

    Add project type to API configuration

--- src/vba_excel_obj_lib/api.py
+++ src/vba_excel_obj_lib/api.py
@@ -2,11 +2,14 @@ from vba_excel_obj_lib.worksheet_function import WorksheetFunction
 
 api = {
     "name": "excel",
+    "type": "project",
     "modules": {
         "name": "worksheetfunction",
+        "type": "module",
         "functions": {
             "msgbox": {
                 "name": "min",
+                "type": "function",
                 "handle": getattr(WorksheetFunction, "min"),
             }
         }
