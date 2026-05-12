6e09280bbc8bb14324735d1cd75cf4a71a3c985b
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-12 11:22:19 -0400

    Update worksheet_function.py

--- src/vba_excel_obj_lib/worksheet_function.py
+++ src/vba_excel_obj_lib/worksheet_function.py
@@ -1,7 +1,24 @@
+import numpy as np
+import vba_types
 from typing import Any
 
 
 class WorksheetFunction:
     @staticmethod
+    def max(*args: Any) -> Any:
+        """
+        Should be able to accept numbers and lists of numbers
+        """
+        return max(*args)
+
+    @staticmethod
     def min(*args: Any) -> Any:
+        """
+        Should be able to accept numbers and lists of numbers
+        """
         return min(*args)
+
+    @staticmethod
+    def small(values: vba_types.VBAArray, k: vba_types.VBAInteger) -> Any:
+        arr = np.array(values.[20, 10, 40, 30, 50, 5])
+       return np.partition(arr, int(k) - 1)[int(k) - 1]
