c40aab1475f992a30264f4be67bb8a148f764ba9
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-12 14:43:05 -0400

    Refactor worksheet function handling in api.py
    Refactored worksheet function import and simplified API structure.

--- src/vba_excel_obj_lib/api.py
+++ src/vba_excel_obj_lib/api.py
@@ -1,4 +1,4 @@
-from vba_excel_obj_lib.worksheet_function import WorksheetFunction
+from .worksheet_function import worksheet_function
 arg1 = {
     "name": "arg1",
     "optional": False,
@@ -17,46 +17,6 @@ api = {
     "name": "excel",
     "type": "project",
     "modules": {
-        "worksheetfunction": {
-            "name": "worksheetfunction",
-            "type": "module",
-            "functions": {
-                "max": {
-                    "name": "max",
-                    "type": "function",
-                    "project": "excel",
-                    "module": "worksheetfunction",
-                    "handle": getattr(WorksheetFunction, "max"),
-                    "params": args
-                }
-                "min": {
-                    "name": "min",
-                    "type": "function",
-                    "project": "excel",
-                    "module": "worksheetfunction",
-                    "handle": getattr(WorksheetFunction, "min"),
-                    "params": args
-                },
-                "small": {
-                    "name": "small",
-                    "type": "function",
-                    "project": "excel",
-                    "module": "worksheetfunction",
-                    "handle": getattr(WorksheetFunction, "small"),
-                    "params": [
-                        {
-                            "name": "arg1",
-                            "optional": False,
-                            "default": ""
-                        },
-                        {
-                            "name": "arg2",
-                            "optional": False,
-                            "default": ""
-                        }
-                    ]
-                }
-            }
-        }
+        "worksheetfunction": worksheet_function.api
     }
 }
