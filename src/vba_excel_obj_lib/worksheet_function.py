6919eb0b0f90df864dfafbe464a020c528b740d8
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-12 14:16:02 -0400

    Change return types to VBADouble for functions

--- src/vba_excel_obj_lib/worksheet_function.py
+++ src/vba_excel_obj_lib/worksheet_function.py
@@ -5,29 +5,34 @@ from typing import Any
 
 class WorksheetFunction:
     @staticmethod
-    def average(*args: Any) -> vba_types.VBAInteger:
+    def average(*args: Any) -> vba_types.VBADouble:
         arr = np.array(args._data)
         return arr.average()
 
     @staticmethod
-    def count(*args: Any) -> vba_types.VBAInteger:
+    def count(*args: Any) -> vba_types.VBADouble:
         return vba_types.VBAInteger(len(args))
 
     @staticmethod
-    def max(*args: Any) -> Any:
+    def max(*args: Any) -> vba_types.VBADouble:
         """
         Should be able to accept numbers and lists of numbers
         """
         return max(*args)
 
     @staticmethod
-    def min(*args: Any) -> Any:
+    def min(*args: Any) -> vba_types.VBADouble:
         """
         Should be able to accept numbers and lists of numbers
         """
         return min(*args)
 
     @staticmethod
-    def small(values: vba_types.VBAArray, k: vba_types.VBAInteger) -> Any:
+    def small(values: vba_types.VBAArray, k: vba_types.VBAInteger) -> vba_types.VBADouble:
         arr = np.array(values._data)
         return np.partition(arr, int(k) - 1)[int(k) - 1]
+
+    @staticmethod
+    def stdev(*args: Any) -> vba_types.VBADouble:
+        arr = np.array(args._data)
+        return arr.average()
