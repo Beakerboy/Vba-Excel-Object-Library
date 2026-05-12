67d6eda8dc63048a0a250725874a6d8e43ab1465
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-12 15:13:38 -0400

    Update test_worksheet_function.py

--- tests/test_worksheet_function.py
+++ tests/test_worksheet_function.py
@@ -3,9 +3,12 @@ import vba_types
 from vba_excel_obj_lib.worksheet_function import WorksheetFunction
 
 
+array_of_ints = vba_types.VBAArray(vba_types.VBAInteger(1),
+                                   vba_types.VBAInteger(2),
+                                   vba_types.VBAInteger(3))
 @pytest.mark.parametrize(
     "input, expected", [
-        (vba_types.VBAArray(1, 2, 3), vba_types.VBADouble(3.0)),
+        (array_of_ints, vba_types.VBADouble(3.0)),
     ])
 def test_max(input, expected) -> None:
     result = WorksheetFunction.max(input)
@@ -13,20 +16,22 @@ def test_max(input, expected) -> None:
 
 
 def test_min() -> None:
-    assert WorksheetFunction.min(vba_types.VBAArray(1, 2, 3)) == vba_types.VBADouble(1.0)
+    assert WorksheetFunction.min(array_of_ints) == vba_types.VBADouble(1.0)
 
 
 def test_average_array() -> None:
-    assert WorksheetFunction.average(vba_types.VBAArray(1, 2, 3)) == vba_types.VBADouble(2.0)
+    assert WorksheetFunction.average(array_of_ints) == vba_types.VBADouble(2.0)
 
 
 def test_average_ints() -> None:
-    assert WorksheetFunction.average(1, 2, 3) == vba_types.VBADouble(2.0)
+    assert WorksheetFunction.average(vba_types.VBAInteger(1),
+                                     vba_types.VBAInteger(2),
+                                     vba_types.VBAInteger(3)) == vba_types.VBADouble(2.0)
 
 
 def test_average_mix() -> None:
-    assert WorksheetFunction.average(vba_types.VBAArray(1, 2), 3) == vba_types.VBADouble(2.0)
+    assert WorksheetFunction.average(vba_types.VBAArray(vba_types.VBAInteger(1), vba_types.VBAInteger((2), vba_types.VBAInteger(3)) == vba_types.VBADouble(2.0)
 
 
 def stdev() -> None:
-    assert WorksheetFunction.stdev(vba_types.VBAArray(1, 2, 3)) == vba_types.VBADouble(1.0)
+    assert WorksheetFunction.stdev(array_of_ints) == vba_types.VBADouble(1.0)
