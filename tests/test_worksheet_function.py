c751ff4346bac44c7d6a89059fa6bdd1aba7673a
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-12 15:06:00 -0400

    Rename test function for mixed input case

--- tests/test_worksheet_function.py
+++ tests/test_worksheet_function.py
@@ -24,7 +24,7 @@ def test_average_ints() -> None:
     assert WorksheetFunction.average(1, 2, 3) == vba_types.VBADouble(2.0)
 
 
-def test_average_ints() -> None:
+def test_average_mix() -> None:
     assert WorksheetFunction.average(vba_types.VBAArray(1, 2), 3) == vba_types.VBADouble(2.0)
 
 
