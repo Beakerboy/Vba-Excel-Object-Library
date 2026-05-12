b0b31384fe44da7a1efeac633fedf4bd653d366e
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-12 14:57:39 -0400

    Update test_worksheet_function.py

--- tests/test_worksheet_function.py
+++ tests/test_worksheet_function.py
@@ -1,14 +1,20 @@
+import pytest
 import vba_types
 from vba_excel_obj_lib.worksheet_function import WorksheetFunction
 
 
-def test_max() -> None:
-    assert WorksheetFunction.max(vba_types.VBAArray(1, 2, 3)) == 1
+@pytest.mark.parametrize(
+    "input, expected", [
+        (vba_types.VBAArray(1, 2, 3), vba_types.VBADouble(3.0)),
+    ])
+def test_max(input, expected) -> None:
+    result = WorksheetFunction.max(input)
+    assert result == expected
 
 
 def test_min() -> None:
-    assert WorksheetFunction.min(vba_types.VBAArray(1, 2, 3)) == 1
+    assert WorksheetFunction.min(vba_types.VBAArray(1, 2, 3)) == vba_types.VBADouble(1.0)
 
 
 def test_average() -> None:
-    assert WorksheetFunction.average(vba_types.VBAArray(1, 2, 3)) == 2.0
+    assert WorksheetFunction.average(vba_types.VBAArray(1, 2, 3)) == vba_types.VBADouble(2.0)
