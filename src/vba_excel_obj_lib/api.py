3f1a794e3636571758b8f6b480290a7bb3f8ff65
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-12 11:07:57 -0400

    Update api.py

--- src/vba_excel_obj_lib/api.py
+++ src/vba_excel_obj_lib/api.py
@@ -1,4 +1,17 @@
 from vba_excel_obj_lib.worksheet_function import WorksheetFunction
+arg1 = {
+    "name": "arg1",
+    "optional": False,
+    "default": ""
+}
+args = [arg1]
+i = 2
+for n in range(29):
+    args.append({
+        "name": "arg2",
+        "optional": True,
+        "default": ""
+    })
 
 api = {
     "name": "excel",
@@ -14,18 +27,7 @@ api = {
                     "project": "excel",
                     "module": "worksheetfunction",
                     "handle": getattr(WorksheetFunction, "min"),
-                     "params": [
-                         {
-                            "name": "arg1",
-                            "optional": False,
-                            "default": ""
-                        },
-                        {
-                            "name": "arg2",
-                            "optional": True,
-                            "default": ""
-                        }
-                     ]
+                     "params": args
                 }
             }
         }
