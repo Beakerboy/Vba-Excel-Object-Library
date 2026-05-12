f8b9e3a17718fb663a1a2a2b2ad807cc13638d0f
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-12 14:53:48 -0400

    Update test_worksheet_function.py

--- tests/test_worksheet_function.py
+++ tests/test_worksheet_function.py
@@ -1,5 +1,14 @@
+import vba_types
 from vba_excel_obj_lib.worksheet_function import WorksheetFunction
 
 
+def test_max() -> None:
+    assert WorksheetFunction.max(vba_types.VBAArray(1, 2, 3)) == 1
+
+
 def test_min() -> None:
-    assert WorksheetFunction.min(1, 2, 3) == 1
+    assert WorksheetFunction.min(vba_types.VBAArray(1, 2, 3)) == 1
+
+
+def test_average() -> None:
+    assert WorksheetFunction.average(vba_types.VBAArray(1, 2, 3)) == 2.0
