ec7cc45ff45d8bb4a5b748a6059ef9150d76655d
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-13 12:41:55 -0400

    Update worksheet_function.py

--- src/vba_excel_obj_lib/worksheet_function.py
+++ src/vba_excel_obj_lib/worksheet_function.py
@@ -9,7 +9,8 @@ class WorksheetFunction:
         vals = []
         for arg in args:
             if isinstance(arg, vba_types.VBAArray):
-                vals.extend(arg)
+                new_list = list(map(lambda x: x.value, arg))
+                vals.extend(new_list)
             else:
                 vals.append(arg.value)
         arr = np.array(vals)
