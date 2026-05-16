827111622d51fa5fb02e83cc373f66951a166b1e
Kevin Nowaczyk <beakerboy99@yahoo.com>
2026-05-15 22:55:53 -0400

    Fix mapping of VBAArray values in worksheet functions

--- src/vba_excel_obj_lib/worksheet_function.py
+++ src/vba_excel_obj_lib/worksheet_function.py
@@ -9,7 +9,7 @@ class WorksheetFunction:
         vals = []
         for arg in args:
             if isinstance(arg, vba_types.VBAArray):
-                new_list = list(map(lambda x: x.value, arg))
+                new_list = list(map(lambda x: x.value, arg.value))
                 vals.extend(new_list)
             else:
                 vals.append(arg.value)
@@ -21,7 +21,7 @@ class WorksheetFunction:
         vals = []
         for arg in args:
             if isinstance(arg, vba_types.VBAArray):
-                new_list = list(map(lambda x: x.value, arg))
+                new_list = list(map(lambda x: x.value, arg.value))
                 vals.extend(new_list)
             else:
                 vals.append(arg.value)
@@ -35,7 +35,7 @@ class WorksheetFunction:
         vals = []
         for arg in args:
             if isinstance(arg, vba_types.VBAArray):
-                new_list = list(map(lambda x: x.value, arg))
+                new_list = list(map(lambda x: x.value, arg.value))
                 vals.extend(new_list)
             else:
                 vals.append(arg.value)
@@ -62,7 +62,7 @@ class WorksheetFunction:
         vals = []
         for arg in args:
             if isinstance(arg, vba_types.VBAArray):
-                new_list = list(map(lambda x: x.value, arg))
+                new_list = list(map(lambda x: x.value, arg.value))
                 vals.extend(new_list)
             else:
                 vals.append(arg.value)
